3de1e8b30e2f71075b7962e47ece5068dde0f82a
Ettore <ettore.barbosa@gmail.com>
2022-05-27 11:29:37 -0300

    migarations fixed

--- prisma/migrations/20220524165817_create_contatos/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Contatos" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "nome" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "telefone" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL,
-    "updatedAt" DATETIME NOT NULL
-);
